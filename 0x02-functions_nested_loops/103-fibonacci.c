475d0a8652a1b7db0f31f88c763e94afabb31e2c
Shallom Kanyori <shallomkanyori@gmail.com>
2023-03-21 19:24:44 +0000

    Fix code to output correct sum

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -20,20 +20,20 @@ int main(void)
  */
 void print_even_sum_fibonacci(void)
 {
-	int even_sum = 1;
+	int even_sum = 0;
 	int n_1 = 1;
 	int n_2 = 2;
-	int f_sum = 3;
+	int c;
 
-	while (f_sum < 4000000)
+	while (n_2 <= 4000000)
 	{
+		if (n_2 % 2 == 0)
+			even_sum += n_2;
+
+		c = n_1 + n_2;
+
 		n_1 = n_2;
-		n_2 = f_sum;
-
-		if (f_sum % 2 == 0)
-			even_sum += f_sum;
-
-		f_sum = n_1 + n_2;
+		n_2 = c;
 	}
 
 	printf("%d\n", even_sum);

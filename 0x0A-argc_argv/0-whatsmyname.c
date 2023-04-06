5ab4e3067a83f66c98376d2dfe4e48e62debb743
Shallom Kanyori <shallomkanyori@gmail.com>
2023-04-06 12:20:44 +0000

    Add C program that prints its name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints the name of this program
+ * @argc: the number of arguments
+ * @argv: the arguments
+ *
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	(void)argc;
+
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
--- 0x0A-argc_argv/README.md
+++ 0x0A-argc_argv/README.md
@@ -1,1 +1,3 @@
 ## C - argc, argv
+
+- **Task 0: It ain't what they call you, it's what you answer to.** Write a program that prints its name, followed by a new line. You shouldn't remove the path before the name of the program.

dc747be4432a491f20cb6bcb55f2538305deeb34
Shallom Kanyori <shallomkanyori@gmail.com>
2023-04-06 13:22:08 +0000

    Add C program that adds positive numbers
    Add a C program that takes in numbers as arguments and prints the result
    of adding them. If any of the numbers contain non-digit symbols, it
    prints 'Error' and returns 1.

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * main - adds positive numbers
+ * @argc: the number of program arguments
+ * @argv: the program arguments
+ *
+ * Return: 1 if one of the numbers contains symbols that are not digits,
+ * otherwise 0.
+ */
+int main(int argc, char *argv[])
+{
+	int res = 0;
+	int num;
+	int i;
+
+	for (i = 1; i < argc; i++)
+	{
+		num = atoi(argv[i]);
+
+		if (strcmp(argv[i], "0") != 0 && num <= 0)
+		{
+			puts("Error");
+			return (1);
+		}
+
+		res += num;
+	}
+
+	printf("%d\n", res);
+	return (0);
+}

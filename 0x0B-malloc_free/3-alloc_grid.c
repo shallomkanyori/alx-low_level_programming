4d28791269cb5a013cbb5fbdc82c41fc2328cb40
Shallom Kanyori <shallomkanyori@gmail.com>
2023-04-11 09:56:48 +0000

    Add C function to create and initialize 2D array
    Add a C function that creates a 2D array and initializes it with 0s then
    returns a pointer to it or NULL on failure.

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * alloc_grid - creates and initializes a 2D integer array
+ * @width: the width of the grid(2D array)
+ * @height: the height of the grid
+ *
+ * Return: A pointer to a 2D integer array. Returns NULL on failure of if
+ * width or height are less than or equal to 0.
+ */
+int **alloc_grid(int width, int height)
+{
+	int **res;
+	int row;
+	int col;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+
+	res = malloc(sizeof(int *) * height);
+
+	if (res == NULL)
+		return (NULL);
+
+	for (row = 0; row < height; row++)
+	{
+		res[row] = malloc(sizeof(int) * width);
+
+		if (res[row] == NULL)
+			return (NULL);
+	}
+
+	for (row = 0; row < height; row++)
+		for (col = 0; col < width; col++)
+			res[row][col] = 0;
+
+	return (res);
+}

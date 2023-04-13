efde0dc1f814abc43ec498042f1216caccfca0df
Shallom Kanyori <shallomkanyori@gmail.com>
2023-04-13 08:18:35 +0000

    Add C function that concatenated two strings
    Add a C function that returns a string containing the concatenation of
    the first string and at most n bytes from the second string.

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,44 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * string_nconcat - concatenates s1 with at most n bytes from s2
+ * @s1: the first string
+ * @s2: the second string
+ * @n: the number of bytes to concatenate
+ *
+ * Return: A pointer to the new string if malloc is successful.
+ * Otherwise, NULL.
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *res;
+	int i;
+	int j;
+	int len1;
+	int len2;
+
+	if (s1 == NULL)
+		s1 = "";
+
+	if (s2 == NULL)
+		s2 = "";
+
+	len1 = strlen(s1);
+	len2 = strlen(s2);
+
+	res = malloc(sizeof(char) * (len1 + len2) + 1);
+
+	if (res == NULL)
+		return (NULL);
+
+	for (i = 0; s1[i]; i++)
+		res[i] = s1[i];
+
+	for (j = 0; s2[j] && (n - j) > 0; j++, i++)
+		res[i] = s2[j];
+
+	res[i] = '\0';
+	return (res);
+}
--- 0x0C-more_malloc_free/README.md
+++ 0x0C-more_malloc_free/README.md
@@ -1,3 +1,4 @@
 ## C - More malloc, free
 
-- **Task 0: Trust no one.** Write a function that allocates memory using `malloc` and returns a pointer to the allocated memory. If `malloc` fails, it should cause normal process termination with a status value of `98`..
+- **Task 0: Trust no one.** Write a function that allocates memory using `malloc` and returns a pointer to the allocated memory. If `malloc` fails, it should cause normal process termination with a status value of `98`.
+- **Task 1: string\_nconcat.** Write a function that concatenates two strings and returns a pointer to the new string which contains the first string concatenated with at most `n` bytes from the second string, null terminated. It should return `NULL` if it fails. If `NULL` is passed, treat it as an empty string.

aeb9af30707fbb60c05c0c7df460b80cd2a3a776
Shallom Kanyori <shallomkanyori@gmail.com>
2023-04-17 10:25:00 +0000

    Create a struct representing a dog
    Create a struct representing a dog with the elements name, age and
    owner.

--- 0x0E-structures_typedef/README.md
+++ 0x0E-structures_typedef/README.md
@@ -1,1 +1,3 @@
 ## C - Structures, typedef
+
+- **Task 0: Poppy.** Define a new type `struct dog` with the following elements: `name`, type = `char *`; `age`, type = `float`; `owner`, type = `char *`.
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - A dog
+ * @name: the name of the dog
+ * @age: the age of the dog
+ * @owner: the owner of the dog
+ *
+ * Description: A struct representing a dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* DOG_H */

6f7b3235fe2c6bb7acae6ba3e9a8ff2594b5164b
Shallom Kanyori <shallomkanyori@gmail.com>
2023-04-19 11:34:03 +0000

    Fix code to make sure operator is one character

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -24,7 +24,7 @@ int (*get_op_func(char *s))(int, int)
 	i = 0;
 	while (s && i < 5)
 	{
-		if (*(ops[i].op) == *(s))
+		if (*(ops[i].op) == *(s) && *(s + 1) == '\0')
 			return (ops[i].f);
 
 		i++;

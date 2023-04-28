818d5f7320c24901db80b74134f230d32c39baab
Shallom Kanyori <shallomkanyori@gmail.com>
2023-04-28 08:25:47 +0000

    Add C function that adds node at beginning of list
    Add a C function that adds a new node at the beginning of a singly
    linked integer list.

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * add_nodeint - adds a new node at the beginning of a listint_t list
+ * @head: the pointer to the pointer to the head node
+ * @n: the integer data of the new node
+ *
+ * Return: the address of the new element, or NULL if it failed.
+ */
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *res = malloc(sizeof(listint_t));
+
+	if (res == NULL)
+		return (NULL);
+
+	res->n = n;
+	res->next = *head;
+	*head = res;
+
+	return (res);
+}
--- 0x13-more_singly_linked_lists/README.md
+++ 0x13-more_singly_linked_lists/README.md
@@ -3,3 +3,5 @@
 - **Task 0: Print list.** Write a function that prints all the elements of a `listint_t` list and returns the number of nodes.
 
 - **Task 1: List length.** Write a function that returns the number of elements in a linked `listint_t` list.
+
+- **Task 2: Add node.** Write a function that adds a new node at the beginning of a `listint_t` list.

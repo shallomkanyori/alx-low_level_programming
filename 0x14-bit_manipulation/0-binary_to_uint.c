0e717c61723575db2daf7bd3be2a311725b128af
Shallom Kanyori <shallomkanyori@gmail.com>
2023-05-04 07:25:08 +0000

    Add C function that converts binary string to unsigned int

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - converts a binary number to an unsigned int
+ * @b: the binary number as a string
+ *
+ * Return: the unsiged int decimal conversion of b, or 0 if b is NULL or b
+ * contains characters other than 0 or 1.
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int res = 0;
+	unsigned int pow_2 = 1;
+	int i = 0;
+
+	if (b == NULL)
+		return (0);
+
+	while (b[i])
+	{
+		if (b[i] != '0' && b[i] != '1')
+			return (0);
+
+		i++;
+	}
+
+	if (i > 32)
+		return (0);
+
+	i--;
+	while (i >= 0)
+	{
+		res += (b[i] - '0') * pow_2;
+		pow_2 *= 2;
+		i--;
+	}
+
+	return (res);
+}

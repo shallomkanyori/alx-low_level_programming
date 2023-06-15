a0a14235a39b0d680c9ab11a431a383ca6a4a0a0
Shallom Kanyori <shallomkanyori@gmail.com>
2023-06-15 06:54:31 +0000

    Add C function that prints a doubly linked integer list

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - prints all the elements of a dlistint_t list
+ * @h: pointer to the first node in the list
+ *
+ * Return: the number of nodes.
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	const dlistint_t *curr = h;
+	size_t num_nodes = 0;
+
+	while (curr)
+	{
+		printf("%d\n", curr->n);
+		curr = curr->next;
+	}
+
+	return (num_nodes);
+}
--- 0x17-doubly_linked_lists/README.md
+++ 0x17-doubly_linked_lists/README.md
@@ -1,1 +1,6 @@
 ## C - Doubly linked lists
+
+**Task 0: Print list.**
+Write a function that prints all the elements of a `dlistint_t` list.
+- Prototype: `size_t print_dlistint(const dlistint_t *h);`
+- Return: the number of nodes
